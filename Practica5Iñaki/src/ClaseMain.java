cfe18531e7cd3dd2006273be87881a6724c5a5ea
zekan97 <ramirezticker97@gmail.com>
2019-02-19 09:48:48 +0100

    Practica 5 hecha

--- Practica5Iñaki/src/ClaseMain.java
+++ Practica5Iñaki/src/ClaseMain.java
@@ -1,6 +1,9 @@
 
 public class ClaseMain {
 	public static void main(String[] args) {
+		/**
+		 * Esta clase muestra un mensaje
+		 */
 		System.out.println("Hola, esta es la practica 5 o 6");
 		System.out.println(ClaseAdicional.mensaje());
 	}
